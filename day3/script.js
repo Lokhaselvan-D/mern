d19ce41bc128a1acbf4af8a27789663c5a14d026
logu <lokhaselvand.23eie@kongu.edu>
2025-06-19 15:40:29 +0530

    3

--- day3/script.js
+++ day3/script.js
@@ -3,14 +3,14 @@ function play(option) {
     let arr=['Rock','Paper','Scissors'];   
     compurterChoice = arr[Math.floor(Math.random() * 3)]; 
     if (option == compurterChoice) {
-        result.innerHTML = "It's a Tie!";
+        result.innerHTML = "It's a Tie!"+"<br>"+"Your choice: "+option+"<br>" +"computer choice: "+compurterChoice;
         console.log("Computer chose: " + compurterChoice);
         console.log("You chose: " + option);
     }else if ((option == 'Rock' && compurterChoice == 'Scissors') || 
                (option == 'Paper' && compurterChoice == 'Rock') || 
                (option == 'Scissors' && compurterChoice == 'Paper')) {
-        result.innerHTML = "You Win!";
+        result.innerHTML = "You Win!"+"<br>"+"Your choice: "+option+"<br>" +"computer choice: "+compurterChoice;
     } else {
-        result.innerHTML = "You Lose!";
+        result.innerHTML = "You Lose!"+"<br>"+"Your choice: "+option+"<br>" +"computer choice: "+compurterChoice;
     }   
 }
